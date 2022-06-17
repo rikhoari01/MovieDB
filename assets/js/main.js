7bed807b9adefdcebc2b3ea2eda99ba5b0ad58f4
rikhoari01 <rikhoari01@gmail.com>
2022-06-17 21:00:56 +0700

    add null search handle

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,36 +54,38 @@ const checkRating = (x) => {
 
 // Search a movie
 $("#searchBtn").click(() => {
-  $("#movie-list").empty();
-  $("#loading").removeClass("hidden");
+  if($("#searchInput").val() != "") {
+    $("#movie-list").empty();
+    $("#loading").removeClass("hidden");
 
-  let search = $("#searchInput").val();
+    let search = $("#searchInput").val();
 
-  $.get(`https://api.themoviedb.org/4/search/movie?api_key=1d2c306bdbd50caa1c4dc11301c06f4c&query=${search}`, (data) => {
-    const results = data.results;
+    $.get(`https://api.themoviedb.org/4/search/movie?api_key=1d2c306bdbd50caa1c4dc11301c06f4c&query=${search}`, (data) => {
+      const results = data.results;
 
-    results.map((result) => {
-      $("#loading").addClass('hidden');
-      $("#movie-list").append(
-        `<article id='${result.id}' class='w-[calc(100%/2.05)] md:w-[calc(100%/4.1)] lg:w-[calc(100%/5)] relative inline-block p-[5px]'>`+
-          "<div class='poster group rounded w-full h-full relative overflow-hidden pt-[140%] cursor-pointer'>"+
-            `<img src='https://image.tmdb.org/t/p/w500${result.poster_path}' alt='${result.title}' class='w-full h-full absolute object-cover mt-[-140%]'>`+
-            "<div class='w-full h-full bg-[rgba(0,0,0,0.35)] absolute top-0 left-0 opacity-0 transition-all ease-in-out group-hover:opacity-100'></div>"+
-            `<div class='absolute bg-white top-[5px] left-[5px] overflow-hidden rounded text-sm py-[4px] px-[6px] font-semibold'><i class='fa fa-star text-yellow-400'></i> ${checkRating(result.vote_average)}</div>`+
-            "<div class='w-full h-full absolute top-[calc(50%-12px)] right-0 opacity-0 transition-all ease-in-out group-hover:opacity-100'>"+
-              "<div class='bg-blue-700 w-max px-5 py-2 rounded text-white mx-auto align-middle'>Details</div>"+
+      results.map((result) => {
+        $("#loading").addClass('hidden');
+        $("#movie-list").append(
+          `<article id='${result.id}' class='w-[calc(100%/2.05)] md:w-[calc(100%/4.1)] lg:w-[calc(100%/5)] relative inline-block p-[5px]'>`+
+            "<div class='poster group rounded w-full h-full relative overflow-hidden pt-[140%] cursor-pointer'>"+
+              `<img src='https://image.tmdb.org/t/p/w500${result.poster_path}' alt='${result.title}' class='w-full h-full absolute object-cover mt-[-140%]'>`+
+              "<div class='w-full h-full bg-[rgba(0,0,0,0.35)] absolute top-0 left-0 opacity-0 transition-all ease-in-out group-hover:opacity-100'></div>"+
+              `<div class='absolute bg-white top-[5px] left-[5px] overflow-hidden rounded text-sm py-[4px] px-[6px] font-semibold'><i class='fa fa-star text-yellow-400'></i> ${checkRating(result.vote_average)}</div>`+
+              "<div class='w-full h-full absolute top-[calc(50%-12px)] right-0 opacity-0 transition-all ease-in-out group-hover:opacity-100'>"+
+                "<div class='bg-blue-700 w-max px-5 py-2 rounded text-white mx-auto align-middle'>Details</div>"+
+              "</div>"+
+              "<div class='absolute bottom-0 left-0 w-full'>"+
+                `<h3 class='text-sm md:text-base p-2 text-center text-white p-[3px] bg-[rgba(0,0,0,0.45)] min-h-[35px]'>${result.title}</h3>`+
+              "</div>"+
             "</div>"+
-            "<div class='absolute bottom-0 left-0 w-full'>"+
-              `<h3 class='text-sm md:text-base p-2 text-center text-white p-[3px] bg-[rgba(0,0,0,0.45)] min-h-[35px]'>${result.title}</h3>`+
-            "</div>"+
-          "</div>"+
-        "</article>")
+          "</article>")
+      })
     })
-  })
+  }
 })
 
 $("#searchInput").keypress((e) => {
-  if(e.which == 13) {
+  if($("#searchInput").val() != "" && e.which == 13) {
     $("#movie-list").empty();
     $("#loading").removeClass("hidden");
 
